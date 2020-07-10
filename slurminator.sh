b332db997a1bb3ed1f01ba08334478d4f08a052a
Norman Koch <norman.koch@tu-dresden.de>
2020-07-10 12:38:51 +0200

    added reload parameter

--- slurminator.sh
+++ slurminator.sh
@@ -15,45 +15,49 @@ function slurminator {
         if command -v whiptail &> /dev/null; then
             JOBS=$(for line in $(squeue -u $USER --format "'%A' '%j (%t, %M)'" | sed '1d'); do echo "$line" | tr '\n' ' '; done)
             chosenjob=$(
-                eval "whiptail --title 'Slurminator' --menu 'Test' 16 100 9 $JOBS 'q)' 'quit slurminator'" 3>&2 2>&1 1>&3
+		    eval "whiptail --title 'Slurminator' --menu 'Test' 16 100 9 $JOBS 'r)' 'reload slurminator' 'q)' 'quit slurminator'" 3>&2 2>&1 1>&3
             )
 
             if [[ $chosenjob == 'q)' ]]; then
                 green_text "Ok"
             else
-                whattodo=$(eval "whiptail --title 'Slurminator $chosenjob' --menu 'Test' 16 100 9 's)' 'Show log path' 'w)' 'whypending' 'f)' 'tail -f' 'c)' 'scancel' 'sc)' 'scancel with signal USR1' 'm)' 'go to main menu' 'q)' 'quit slurminator'" 3>&2 2>&1 1>&3)
-                case $whattodo in
-                    "s)")
-                        echo "slurmlogpath $chosenjob"
-                        slurmlogpath $chosenjob
-                    ;;
-                    "f)")
-                        echo "tail -f $(slurmlogpath $chosenjob)"
-                        tail -f $(slurmlogpath $chosenjob)
-                    ;;
-                    "w)")
-                        echo "whypending $chosenjob"
-                        whypending $chosenjob
-                    ;;
-                    "c)")
-                        if (whiptail --title "Really kill $chosenjob" --yesno "Are you sure you want to kill $chosenjob?" 8 78); then
-                            echo "scancel $chosenjob"
-                            scancel $chosenjob && green_text "$chosenjob killed" || red_text "Error killing $chosenjob"
-                        fi
-                    ;;
-                    "m)")
+		if [[ $chosenjob == 'r)' ]]; then
 			slurminator
-                    ;;
-                    "sc)")
-                        if (whiptail --title "Really kill $chosenjob" --yesno "Are you sure you want to kill $chosenjob with USR1?" 8 78); then
-                            echo "scancel --signal=USR1 --batch $chosenjob"
-                            scancel --signal=USR1 --batch $chosenjob && green_text "$chosenjob killed" || red_text "Error killing $chosenjob"
-                        fi
-                    ;;
-                    "q)")
-			green_text "Ok"
-                    ;;
-                esac
+		else
+			whattodo=$(eval "whiptail --title 'Slurminator $chosenjob' --menu 'Test' 16 100 9 's)' 'Show log path' 'w)' 'whypending' 'f)' 'tail -f' 'c)' 'scancel' 'sc)' 'scancel with signal USR1' 'm)' 'go to main menu' 'q)' 'quit slurminator'" 3>&2 2>&1 1>&3)
+			case $whattodo in
+			    "s)")
+				echo "slurmlogpath $chosenjob"
+				slurmlogpath $chosenjob
+			    ;;
+			    "f)")
+				echo "tail -f $(slurmlogpath $chosenjob)"
+				tail -f $(slurmlogpath $chosenjob)
+			    ;;
+			    "w)")
+				echo "whypending $chosenjob"
+				whypending $chosenjob
+			    ;;
+			    "c)")
+				if (whiptail --title "Really kill $chosenjob" --yesno "Are you sure you want to kill $chosenjob?" 8 78); then
+				    echo "scancel $chosenjob"
+				    scancel $chosenjob && green_text "$chosenjob killed" || red_text "Error killing $chosenjob"
+				fi
+			    ;;
+			    "m)")
+				slurminator
+			    ;;
+			    "sc)")
+				if (whiptail --title "Really kill $chosenjob" --yesno "Are you sure you want to kill $chosenjob with USR1?" 8 78); then
+				    echo "scancel --signal=USR1 --batch $chosenjob"
+				    scancel --signal=USR1 --batch $chosenjob && green_text "$chosenjob killed" || red_text "Error killing $chosenjob"
+				fi
+			    ;;
+			    "q)")
+				green_text "Ok"
+			    ;;
+			esac
+		    fi
             fi
         else
             red_text "whiptail not found. Cannot execute slurminator without it"
