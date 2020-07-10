e59228a8740bfd7e4f8a44e3e3fa110e809496bd
Norman Koch <norman.koch@tu-dresden.de>
2020-07-10 10:47:40 +0200

    ftails -> tail -f

--- slurminator.sh
+++ slurminator.sh
@@ -9,7 +9,7 @@ function slurminator {
     if [[ $chosenjob == 'e)' ]]; then
         echo "Ok"
     else
-        whattodo=$(eval "whiptail --title 'Slurminator $chosenjob' --menu 'Test' 16 100 9 's)' 'Show log path' 'f)' 'ftails' 'c)' 'scancel' 'sc)' 'secure cancel (USR1)' 'e)' 'Exit program'" 3>&2 2>&1 1>&3)
+        whattodo=$(eval "whiptail --title 'Slurminator $chosenjob' --menu 'Test' 16 100 9 's)' 'Show log path' 'f)' 'tail -f' 'c)' 'scancel' 's)' 'secure cancel (USR1)' 'e)' 'Exit program'" 3>&2 2>&1 1>&3)
         case $whattodo in
             "s)")
                 echo "slurmlogpath $chosenjob"
@@ -24,7 +24,7 @@ function slurminator {
                     scancel $chosenjob
                 fi
             ;;
-            "sc)")
+            "s)")
                 if (whiptail --title "Really kill $chosenjob" --yesno "Are you sure you want to kill $chosenjob with USR1?" 8 78); then
                     echo "scancel --signal=USR1 --batch $chosenjob"
                     scancel --signal=USR1 --batch $chosenjob
