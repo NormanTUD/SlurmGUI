5589a3416c4be31b5d71454dda4f4e4763d51c4f
Norman Koch <norman.koch@tu-dresden.de>
2020-07-10 12:07:22 +0200

    changed some small things

--- slurminator.sh
+++ slurminator.sh
@@ -15,13 +15,13 @@ function slurminator {
         if command -v whiptail &> /dev/null; then
             JOBS=$(for line in $(squeue -u $USER --format "'%A' '%j (%t, %M)'" | sed '1d'); do echo "$line" | tr '\n' ' '; done)
             chosenjob=$(
-                eval "whiptail --title 'Slurminator' --menu 'Test' 16 100 9 $JOBS 'e)' 'Exit program'" 3>&2 2>&1 1>&3
+                eval "whiptail --title 'Slurminator' --menu 'Test' 16 100 9 $JOBS 'q)' 'quit program'" 3>&2 2>&1 1>&3
             )
 
-            if [[ $chosenjob == 'e)' ]]; then
-                echo "Ok"
+            if [[ $chosenjob == 'q)' ]]; then
+                green_text "Ok"
             else
-                whattodo=$(eval "whiptail --title 'Slurminator $chosenjob' --menu 'Test' 16 100 9 's)' 'Show log path' 'w)' 'whypending' 'f)' 'tail -f' 'c)' 'scancel' 's)' 'scancel with signal USR1' 'e)' 'Exit program'" 3>&2 2>&1 1>&3)
+                whattodo=$(eval "whiptail --title 'Slurminator $chosenjob' --menu 'Test' 16 100 9 's)' 'Show log path' 'w)' 'whypending' 'f)' 'tail -f' 'c)' 'scancel' 'sc)' 'scancel with signal USR1' 'q)' 'quit program'" 3>&2 2>&1 1>&3)
                 case $whattodo in
                     "s)")
                         echo "slurmlogpath $chosenjob"
@@ -41,13 +41,14 @@ function slurminator {
                             scancel $chosenjob && green_text "$chosenjob killed" || red_text "Error killing $chosenjob"
                         fi
                     ;;
-                    "s)")
+                    "sc)")
                         if (whiptail --title "Really kill $chosenjob" --yesno "Are you sure you want to kill $chosenjob with USR1?" 8 78); then
                             echo "scancel --signal=USR1 --batch $chosenjob"
                             scancel --signal=USR1 --batch $chosenjob && green_text "$chosenjob killed" || red_text "Error killing $chosenjob"
                         fi
                     ;;
-                    "e)")
+                    "q)")
+			green_text "Ok"
                     ;;
                 esac
             fi
