245bbb6c309024ff81db88342de20978560a257f
Norman Koch <norman.koch@tu-dresden.de>
2020-07-11 22:45:33 +0200

    changed small things

--- slurminator.sh
+++ slurminator.sh
@@ -59,7 +59,8 @@ function single_job_tasks {
 	fi
 
 	jobname=$(get_job_name $chosenjob)
-	whattodo=$(eval "whiptail --title 'Slurminator >$jobname< ($chosenjob)' --menu 'What to do with job >$jobname< ($chosenjob)' $WIDTHHEIGHT 's)' 'Show log path' $WHYPENDINGSTRING $TAILSTRING $SCANCELSTRING 'm)' 'go to main menu' 'q)' 'quit slurminator'" 3>&2 2>&1 1>&3)
+	whiptailoptions="'s)' 'Show log path' $WHYPENDINGSTRING $TAILSTRING $SCANCELSTRING 'm)' 'go to main menu' 'q)' 'quit slurminator'"
+	whattodo=$(eval "whiptail --title 'Slurminator >$jobname< ($chosenjob)' --menu 'What to do with job >$jobname< ($chosenjob)' $WIDTHHEIGHT $whiptailoptions" 3>&2 2>&1 1>&3)
 	case $whattodo in
 		"s)")
 			echo "slurmlogpath $chosenjob"
@@ -74,7 +75,7 @@ function single_job_tasks {
 			whypending $chosenjob
 			;;
 		"k)")
-			if (whiptail --title "Really kill >$jobname< ($chosenjob)" --yesno "Are you sure you want to kill >$jobname< ($chosenjob)?" 8 78); then
+			if (whiptail --title "Really kill >$jobname< ($chosenjob)?" --yesno "Are you sure you want to kill >$jobname< ($chosenjob)?" 8 78); then
 				echo "scancel $chosenjob"
 				scancel $chosenjob && green_text "$chosenjob killed" || red_text "Error killing $chosenjob"
 			fi
@@ -83,7 +84,7 @@ function single_job_tasks {
 			slurminator
 			;;
 		"c)")
-			if (whiptail --title "Really kill >$jobname< ($chosenjob)" --yesno "Are you sure you want to kill >$jobname< ($chosenjob) with USR1?" 8 78); then
+			if (whiptail --title "Really kill with USR1 >$jobname< ($chosenjob)?" --yesno "Are you sure you want to kill >$jobname< ($chosenjob) with USR1?" 8 78); then
 				echo "scancel --signal=USR1 --batch $chosenjob"
 				scancel --signal=USR1 --batch $chosenjob && green_text "$chosenjob killed" || red_text "Error killing $chosenjob"
 			fi
