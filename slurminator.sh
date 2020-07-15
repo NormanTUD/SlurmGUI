46e89f1bbb065b851dfa1153a00a9342cf34f5a2
Norman Koch <norman.koch@tu-dresden.de>
2020-07-15 11:53:47 +0200

    test for workspaces

--- slurminator.sh
+++ slurminator.sh
@@ -295,7 +295,7 @@ function show_workspace_options {
 		FAILED=1
 	fi
 	if [[ $FAILED == 0 ]]; then
-		existingworkspaces=$(ws_list | perl -e 'my %struct = (); my $current_id = q##; while (<>) { if(m#^id: (.*)$#) { $current_id = $1 } elsif (m#remaining time\s*:\s*(.*)?$#) { $struct{$current_id} = $1 } }; foreach my $key (keys %struct) { print qq#"$key" "$struct{$key}"# }')
+		existingworkspaces=$(ws_list | perl -e 'my %struct = (); my $current_id = q##; while (<>) { if(m#^id: (.*)$#) { $current_id = $1 } elsif (m#remaining time\s*:\s*(.*)?$#) { $struct{$current_id} = $1 } }; foreach my $key (keys %struct) { print qq#"$key" "$struct{$key}" # }')
 		echo "whiptail --title 'Which workspaces to do something with?' --checklist 'Which workspace?' $WIDTHHEIGHT $existingworkspaces"
 		chosenjobs=$(eval "whiptail --title 'Which workspaces to do something with?' --checklist 'Which workspace?' $WIDTHHEIGHT $existingworkspaces" 3>&1 1>&2 2>&3)
 	else
